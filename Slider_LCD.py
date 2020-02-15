3f14d8acd5721264f1851d383e4217f0542827c0
ChristianQC <46302054+ChristianQC@users.noreply.github.com>
2020-02-15 13:55:25 -0500

    Create Slider_LCD.py

---
+++ Slider_LCD.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow ,QSlider , QLCDNumber ,QVBoxLayout
+from PyQt5.QtCore import Qt
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.lcd = QLCDNumber()
+        self.lcd.setFixedWidth(100)
+        self.lcd.setFixedHeight(100)
+        
+        mySlyder = QSlider(Qt.Horizontal,self)
+        mySlyder.setMaximun(254)
+        mySlyder.setMinimun(0)
+		mySlyder.setGeometry(30,40,200,30)
+        mySlyder.valueChanged.connect(self.changedValue)
+        
+        self.setGeometry(50,50,320,200)
+        self.setWindowTitle("Curso RPI4")
+        
+        layout = QVBoxLayout()
+        layout.addWidget(self.lcd)
+        layout.addWidget(mySlider)
+        self.setLayout(layout)
+        
+    def changedValue(self,value):
+        print(value)
+        self.lcd.display(value)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
